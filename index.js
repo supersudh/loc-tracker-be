9a401fee840f7a6733d44394ba827370f154a67e
Sudharsan Ravikumar <supersudh@gmail.com>
2021-08-10 12:55:07 +0530

    completed

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json({ status: 'loc-tracker-be listening at ' + PORT });
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  return res.json({ status: 'ok' });
+});
+
+app.listen(PORT, () => {
+  console.log('loc-tracker-be listening at ' + PORT);
+});
